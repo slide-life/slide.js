77ff9b57f722148b91a86e0cc31bb162860701ec
Jack Dent <jack@jackdent.co.uk>
2014-11-26 17:19:03 -0500

    ES6 modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,43 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   grunt.initConfig({
+    clean: {
+      build: ['build/'],
+      release: ['dist']
+    },
+
+    transpile: {
+      main: {
+        type: 'cjs',
+        files: [{
+          cwd: 'lib/',
+          expand: true,
+          dest: 'build/',
+          src: '**/*.js'
+        }]
+      }
+    },
+
+    browserify: {
+      build: {
+        src: 'build/*.js',
+        dest: 'build/browser.js'
+      }
+    },
+
     concat: {
       options: {
         separator: ';'
       },
       dist: {
-        src: ['bower_components/sjcl/sjcl.js', 'lib/slide-crypto.js', 'lib/slide.js'],
+        src: ['bower_components/sjcl/sjcl.js', 'build/browser.js'],
         dest: 'dist/slide.js'
       }
     }
   });
 
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.registerTask('default', ['concat']);
-}
+  grunt.loadNpmTasks('grunt-es6-module-transpiler');
+  grunt.registerTask('default', ['clean', 'transpile', 'browserify', 'concat']);
+};
