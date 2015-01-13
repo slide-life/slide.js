cf22cec1fdaa3844f2855088108937752ed34591
Matt Neary <neary.matt@gmail.com>
2015-01-13 02:38:59 -0500

    request made separate from instantiation of conversation

--- dist/slide.js
+++ dist/slide.js
@@ -28729,10 +28729,21 @@ var Conversation = function(key, upstream, downstream) {
   this.key = key;
   this.upstream = upstream;
   this.downstream = downstream;
+  var self = this;
   $.post(Slide.endpoint("/conversations"),
     JSON.stringify({key: key, upstream: upstream, downstream: downstream}),
     function(conversation) {
       console.log(conversation);
+      self.id = conversation.id;
+      self.request(['first-name']);
+    });
+};
+
+Conversation.prototype.request = function(blocks) {
+  $.post(Slide.endpoint("/conversations/" + this.id + "/request_content"),
+    JSON.stringify({blocks: blocks}),
+    function(conversation) {
+      console.log("requested", blocks);
     });
 };
 
