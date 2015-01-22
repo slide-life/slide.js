e514e8f10438ba8cce98e25fc462ec1631bbc0a4
Jack Dent <jack@jackdent.co.uk>
2015-01-22 17:06:06 -0500

    Mocha testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,6 +74,15 @@ module.exports = function (grunt) {
         files: ['scss/**/*.scss'],
         tasks: ['sass']
       }
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: 'tests/**/*.js'
+      }
     }
   });
 
@@ -85,7 +94,8 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-mocha-test');
 
   grunt.registerTask('default', ['clean', 'transpile', 'browserify', 'exec', 'concat', 'sass']);
-  grunt.registerTask('test', 'jshint');
+  grunt.registerTask('test', 'mochaTest');
 };
