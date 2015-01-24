634417b738ba908b953f124afd0a0645294fe141
zgao <gaoz@mit.edu>
2015-01-23 20:35:07 -0500

    Removed node-forge dep, use bower version for dep consistency

--- tests/monkey-patches.js
+++ tests/monkey-patches.js
@@ -1,7 +1,7 @@
 GLOBAL.window = {location: {search: '', href: '', pathname: ''}};
 GLOBAL.$ = require('jquery')(require('jsdom').jsdom().parentWindow);
 GLOBAL.$.ajax = require('najax');
-GLOBAL.forge = require('node-forge');
+GLOBAL.forge = require('../bower_components/forge/js/forge.js');
 
 GLOBAL.btoa = function (string) {
   return new Buffer(string).toString('base64');
