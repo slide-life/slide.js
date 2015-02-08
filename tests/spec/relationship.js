56b0129d3dff968d015ad8726b07e48fb0af0512
Jack Dent <jack@jackdent.co.uk>
2015-02-08 14:27:05 -0500

    Messages

--- tests/spec/relationship.js
+++ tests/spec/relationship.js
@@ -1,6 +1,18 @@
 var assert = require('assert');
 var Slide = require('../../lib/slide');
 
+function requestAndRespond (individual, organisation, relationship, data, cbs) {
+  relationship.createConversation('Test conversation', {
+    success: function (conversation) {
+      conversation.sendRequest(individual, ['bank.card'], {
+        success: function (request) {
+          conversation.sendResponse(request, data, cbs);
+        }
+      });
+    }
+  });
+}
+
 describe('Relationship', function () {
   var individual, organisation, relationship;
 
@@ -31,8 +43,16 @@ describe('Relationship', function () {
 
   describe('.createConversation()', function () {
     it('should create a conversation on a relationship', function (done) {
-      relationship.createConversation('Test conversation', {
-        success: function (conversation) {
+      requestAndRespond(individual, organisation, relationship, { 'bank.card': 'Test' }, {
+        success: function () {
+          done();
+        }
+      });
+    });
+
+    it('should fail to create a conversation on a relationship', function (done) {
+      requestAndRespond(individual, organisation, relationship, { 'bank': 'Test' }, {
+        failure: function () {
           done();
         }
       });
