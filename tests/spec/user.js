9bfc6f5194570675efd69192ee5e94d5ecf750d5
Jack Dent <jack@jackdent.co.uk>
2015-02-09 18:00:07 -0500

    User SMS verification

--- tests/spec/user.js
+++ tests/spec/user.js
@@ -1,15 +1,49 @@
 var assert = require('assert');
 var Slide = require('../../lib/slide');
 
+var http = require('http');
+var url = require('url');
+var ngrok = require('ngrok');
+
+var NGROK_AUTH_TOKEN = 'UiKi8KAZGrUOstdifNx7';
+var NGROK_TUNNEL = 'slide-testing-56f2d61a';
+var NGROK_RECEIVER_PORT = 9293;
+var SMS_TEST_NUMBER = 18575762052;
+
 describe('User', function () {
   var user;
 
   before(function (done) {
-    Slide.User.create({ value: 12013514474, type: 'phone' }, 'test_password_123', {
-      success: function (u) {
-        user = u;
+    ngrok.connect({
+      authtoken: NGROK_AUTH_TOKEN,
+      subdomain: NGROK_TUNNEL,
+      port: NGROK_RECEIVER_PORT
+    }, function () {
+      Slide.User.create({ value: SMS_TEST_NUMBER, type: 'phone' }, 'test_password_123', {
+        success: function (u) {
+          user = u;
+          done();
+        }
+      });
+    });
+  });
+
+
+  describe('.verifyIdentifier()', function () {
+    it('should verify the identifier that the user has signed up wih', function (done) {
+      var server = http.createServer(function (request, response) {
+        var data = url.parse(request.url, true).query;
+        var pin = data.Body.split('Your slide verification code is ')[1].split('. It will expire in 5 minutes.')[0];
+
+        assert.equal(pin.length, 6);
+        // TODO: verify number
+
+        response.writeHead(200);
+        response.end();
+
+        server.close();
         done();
-      }
+      }).listen(NGROK_RECEIVER_PORT);
     });
   });
 
@@ -50,5 +84,9 @@ describe('User', function () {
         }
       })
     });
-  })
+  });
+
+  after(function () {
+    ngrok.disconnect();
+  });
 });
